5802d2571f4508760fc9f1135b4eb219b90ce689
Yonatan Karp-Rudin <yonatan.karp@sumup.com>
2023-08-16 23:52:02 +0200

    Init project

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# WeatherAPI-client
+# WeatherAPI client
 A Client library in Kotlin for WeatherApi.com
+
+This is the example library instrumentation as described in the article
+[LINK TO ARTICLE]()
+
+## Authors
+
+- **Yonatan Karp-Rudin** - *Initial work* - [yonatankarp](https://github.com/yonatankarp)
---
+++ build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+    id("maven-publish")
+    id("java")
+    `java-library`
+    kotlin("jvm") version "1.9.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Tests
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
+
+tasks.jar {
+    archiveFileName.set("${project.name}-${project.version}.jar")
+    manifest {
+        attributes(
+            "Implementation-Title" to project.name,
+            "Implementation-Version" to project.version
+        )
+    }
+    from(
+        configurations.runtimeClasspath
+            .get()
+            .filter { it.exists() }
+            .map { if (it.isDirectory) it else zipTree(it) }
+    )
+    exclude("META-INF/*.SF", "META-INF/*.DSA", "META-INF/*.RSA", "META-INF/LICENSE.txt")
+    includeEmptyDirs = false
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.yonatankarp"
+            artifactId = project.name
+            version = project.version.toString()
+
+            from(components["java"])
+        }
+
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/yonatankarp/WeatherAPI-client")
+                credentials {
+                    username = project.findProperty("gpr.user")?.toString() ?: System.getenv("GITHUB_ACTOR")
+                    password = project.findProperty("gpr.key")?.toString() ?: System.getenv("GITHUB_TOKEN")
+                }
+            }
+        }
+    }
+}
